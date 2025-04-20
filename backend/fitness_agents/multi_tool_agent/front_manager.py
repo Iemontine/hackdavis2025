e49e9886cfc86fe1745020032db37bf2509a274d
Iemontine <computereality@gmail.com>
2025-04-19 20:32:35 -0700

    Updte promt

--- backend/fitness_agents/multi_tool_agent/front_manager.py
+++ backend/fitness_agents/multi_tool_agent/front_manager.py
@@ -31,11 +31,19 @@ def create_profile_json(height: str, weight: str, fitness_level: str, workout_ti
     }
     return str(profile).replace("'", '"')  # Convert to JSON string
 
+def close_session(session):
+    """Closes the session."""
+    try:
+        session.close()
+    except Exception as e:
+        print(f"Error closing session: {e}")
+
 front_agent = Agent(
     name="Front_Manager",
     model=AGENT_MODEL, # Specifies the underlying LLM
     description="Interacts with the user and identifies their needs", # Crucial for delegation later
     instruction="You are a helpful fitness assistant. Your primary goal is to identify the user's fitness needs and goals. "
+                "Ask your questions one by one. Do not ask all questions at once. Ensure you have an answer before proceeding to the next question. "
                 "Ask the user their current height and weight."
                 "Ask the user's fitness level. Prompt to identify as beginner, intermediate, or advanced."
                 "Ask the user what is their comfotable length of time to work out each session."
